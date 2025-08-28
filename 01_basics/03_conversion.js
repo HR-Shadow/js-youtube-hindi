f587a1a05d478698f9f4a1c0ad6e04395eca11a2
Harsh Rajak <harshrajak231@gmail.com>
2025-08-28 13:19:08 +0000

    convertion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,8 @@
+let inlogged = "33" ; 
+
+console.log(inlogged) ; 
+console.log(typeof (inlogged)) 
+let InNumber = Number(inlogged) 
+console.log(typeof InNumber)
+let Islogin = "Harsh" 
+console.log(Boolean(Islogin))
